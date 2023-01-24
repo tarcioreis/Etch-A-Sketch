9b5ca615cc1315f786b2f69040d2986d40375433
Tarcio Reis <souzareistarcio@gmail.com>
2023-01-24 16:17:53 -0300

    add pen options erase and clear board functions

--- script.js
+++ script.js
@@ -1,16 +1,65 @@
-//const size = window.prompt();
+let size = 16;
+let color = "black";
+let pens = ["pen", "normal", "bold", "bolder"];
 
 const container = document.querySelector(".container");
-container.style.display = "grid";
 
-function changeColor (event) {
-    event.target.style.backgroundColor = "black";
+const board = document.querySelector(".board");
+board.style.display = "grid";
+
+const eraser = document.createElement("button");
+eraser.innerText = "eraser";
+
+const clear = document.createElement("button");
+clear.innerText = "clear";
+
+const selectPen = document.createElement("select");
+
+for (const pen of pens) {
+    let option = document.createElement("option");
+    option.innerText = pen;
+    option.id = pen;
+    selectPen.appendChild(option);
 }
 
-// create grid
-function render (size) {
-    container.style.gridTemplateColumns = `repeat(${size}, 0.5fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 0.5fr)`;
+function penOptions (event) {
+    const value = event.target.value;
+    if (value === "normal") {
+        color = "black";
+        size = 64;
+        clearBoard();
+        render();
+    }
+    if (value === "bold") {
+        color = "black";
+        size = 32;
+        clearBoard();
+        render();
+    }
+
+    if (value === "bolder") {
+        color = "black";
+        size = 16;
+        clearBoard();
+        render();
+    }
+}
+
+function erase () { color = "white"; }
+
+function clearBoard () {
+    board.innerHTML = "";
+    render();
+}
+
+function changeColor (event) {
+    event.target.style.backgroundColor = `${color}`;
+}
+
+// create grid and render board
+function render () {
+    board.style.gridTemplateColumns = `repeat(${size}, 0.5fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 0.5fr)`;
     
     for (let i = 0; i < size * size; i++) {
         let div = document.createElement("div");
@@ -18,12 +67,19 @@ function render (size) {
         div.id = i;
         //div.innerText = i;
         div.onmouseover = changeColor;
-        container.appendChild(div);
+        board.appendChild(div);
     }
 }
 
-function test () {
-    render(16);
+container.appendChild(eraser);
+container.appendChild(selectPen);
+container.appendChild(clear);
+
+function run () {
+    render();
+    eraser.onclick = erase;
+    selectPen.onclick = penOptions;
+    clear.onclick = clearBoard;
 }
 
-test();
+run();
